4c3be663f58692921eb78520ec9cdbf3c37b13f0
pyjavathon <86497379+pyjavathon@users.noreply.github.com>
2023-03-18 22:35:15 +0900

    .

--- src/main/java/com/syaj/OneIt/OneItApplication.java
+++ src/main/java/com/syaj/OneIt/OneItApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OneItApplication {
+public class OneItApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneItApplication.class, args);
