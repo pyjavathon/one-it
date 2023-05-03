bcc51c41a8250b9a18e4cd7026502433ca66a094
yunayeon <atman1220@myatman.com>
2023-05-03 11:10:50 +0900

    jwt bearer 빼고 header에서 auth 할 수 있도록 수정

--- src/main/java/com/syaj/OneIt/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/syaj/OneIt/jwt/JwtAuthenticationFilter.java
@@ -4,23 +4,18 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.jsonwebtoken.IncorrectClaimException;
 import lombok.RequiredArgsConstructor;
 
 
@@ -35,37 +30,33 @@ private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFi
 
 	
 	@Override
-	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException,ServletException{
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException,ServletException{
 		
-		String accessToken = resolveToken(request);
-		String requestURI = request.getRequestURI();
-		
-		try {
-			if(accessToken != null && tokenProvider.validateToken(accessToken)) {
-				Authentication authentication = tokenProvider.getAuthentication(accessToken);//토큰 유효하면 athentication 객체 받아와서 security context에 set
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-				logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}",authentication.getName(),requestURI);
-			}
-		}catch (IncorrectClaimException e) {
-			SecurityContextHolder.clearContext();
-			logger.debug("Invalid JWT token");
-			response.sendError(403);
-		}catch(UsernameNotFoundException e) {
-			SecurityContextHolder.clearContext();
-			logger.debug("Can't find user");
-			response.sendError(403);
+		String jwt = resolveToken((HttpServletRequest) request);
+		if (!jwt.isEmpty())
+			jwt.replace("Bearer ", "");
+
+		if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+			Authentication authentication = tokenProvider.getAuthentication(jwt);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
-		
-		filterChain.doFilter(request, response);
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT"); // 허용할 request http METHOD : POST, GET,
+																					// DELETE, PUT
+		res.setHeader("Access-Control-Allow-Headers",
+				"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+		if (req.getMethod().equals(HttpMethod.OPTIONS.name())) {
+			res.setStatus(HttpStatus.OK.value());
+		} else {
+			chain.doFilter(request, response);
+		}
 	}
 
 	// Request Header 에서 토큰 정보를 꺼내오기 위한 resolveToken 메소드 추가
 	private String resolveToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-			return bearerToken.substring(7);
-		}
-
-		return null;
+		return request.getHeader(AUTHORIZATION_HEADER);
 	}
 }
